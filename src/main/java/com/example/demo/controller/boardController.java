8d7d4cb145bc2cadd1b67528069d974a9850da52
w8er <jhjj8888@naver.com>
2022-11-20 16:32:22 +0900

    git config --global core.autocrlf true

--- src/main/java/com/example/demo/controller/boardController.java
+++ src/main/java/com/example/demo/controller/boardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,15 +20,19 @@ public class boardController {
     @GetMapping("/board/write")         // 어떤 url로 접근할거냐? // localhost:8080/board/write
     public String boardWriteForm() {
 
-        return "Example";
+        return "boardwrite";
     }
 
     @PostMapping("/board/writePro")
-    public String boardWritePro(Board board) {
+    public String boardWritePro(Board board, Model model) {
 
-    boardService.write(board);
+        boardService.write(board);
 
-        return "";
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -44,6 +49,37 @@ public class boardController {
 
         model.addAttribute("board", boardService.boardView(id));
         return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model) {
+
+        Board boardTemp = boardService.boardView(id);   // 기존의 내용을 가져온다.
+        boardTemp.setTitle(board.getTitle());           // 입력한 타이틀을 덧씌운다.
+        boardTemp.setContent(board.getContent());       // 입력한 콘텐츠를 덧씌운다.
+
+        boardService.write(boardTemp);                  // 보드서비스 호출해서 boardTemp의 내용을 write한다.
+
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 }
 
